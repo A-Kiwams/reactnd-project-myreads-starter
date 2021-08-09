feb369b1d5a671df9d600d27e9d7d1a96ee7a815
A-Kiwams <alhassankiwamdeen@gmail.com>
2021-08-09 15:42:57 +0100

    new commit

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,3 +2,30 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import * as BooksAPI from "./BooksAPI";
+
+export const LIST_OPTIONS = [
+    {
+      value: "move",
+      label: "Move to...",
+      disabled: true,
+    },
+    {
+      value: "currentlyReading",
+      label: "Currently Reading",
+      toShow: true,
+    },
+    {
+      value: "wantToRead",
+      label: "Want to Read",
+      toShow: true,
+    },
+    {
+      value: "read",
+      label: "Read",
+      toShow: true,
+    },
+    {
+      value: "none",
+      label: "None",
+    },
+  ];
